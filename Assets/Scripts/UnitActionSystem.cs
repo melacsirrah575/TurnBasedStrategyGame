d7ab2089bd12647956085052ae18f3ec4fac6a93
melacsirrah575 <melacsirrah575@gmail.com>
2022-09-29 15:34:42 -0300

    Added visual to determine selected unit. Also switched to building for HTML5.

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,13 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ActionSystem
 {
+    //THIS SCRIPT IS A SINGLETON
     public class UnitActionSystem : MonoBehaviour
     {
+        public static UnitActionSystem Instance { get; private set; }
+
+        public event EventHandler OnSelectedUnitChanged;
+
         [SerializeField] private Unit selectedUnit;
         [SerializeField] private LayerMask unitLayerMask;
+
+        #region Public Methods
+        public Unit GetSelectedUnit()
+        {
+            return selectedUnit;
+        }
+
+        #endregion
+
+        #region Private Methods
+        //Turning this script into a Singleton
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogError($"There is more than one UnitActionSystem {transform} - {Instance}");
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
 
         private void Update()
         {
@@ -26,12 +54,27 @@ namespace ActionSystem
             {
                 if (hit.transform.TryGetComponent<Unit>(out Unit selectedUnit))
                 {
-                    this.selectedUnit = selectedUnit;
+                    SetSelectedUnit(selectedUnit);
                     return true;
                 }
             }
 
             return false;
         }
+
+        private void SetSelectedUnit(Unit unit)
+        {
+            selectedUnit = unit;
+            OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+
+            //COMMENTED CODE BELOW EXPLAINS WHAT ABOVE ONSELECTUNITCHANGED IS DOING
+            //if (OnSelectedUnitChanged != null)
+            //{
+            //    //'this' is a reference to the object that fired off the event
+            //    OnSelectedUnitChanged(this, EventArgs.Empty);
+            //}
+        }
+
+        #endregion
     }
 }
